1ee85232b8615466bf0fc8e1b4f4c9b666a5a973
Zach Stanley <zcstanley24@gmail.com>
2025-07-12 01:32:59 -0500

    init

---
+++ src/main/java/com/zach/market_monitor/MarketMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.zach.market_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zach/market_monitor/MarketMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.market_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
