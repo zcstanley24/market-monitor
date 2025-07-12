1f3be0ee3e5339542760714b15ed521e94cd9d3b
Zach Stanley <zcstanley24@gmail.com>
2025-07-12 17:36:02 -0500

    Added default frontend application

---
+++ market-monitor/src/main/java/com/zach/market_monitor/APIService.java
@@ -0,0 +1,40 @@
+package com.zach.market_monitor;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class APIService {
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("https://api.twelvedata.com/")
+            .defaultHeader("Authorization", "apikey 98ae839017c04dffb5070af584ce6205")
+            .build();
+
+    public String getCurrentPrice(String stockSymbol) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/price")
+                        .queryParam("symbol", stockSymbol)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    public String getQuote(String stockSymbol, String interval) {
+        String[] intervals = {"1min", "5min", "15min", "30min", "45min", "1h", "2h", "4h", "1day", "1week", "1month"};
+        String requestInterval = (!Arrays.asList(intervals).contains(interval)) ? "1day" : interval;
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/quote")
+                        .queryParam("symbol", stockSymbol)
+                        .queryParam("interval", requestInterval)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+}
---
+++ market-monitor/src/main/java/com/zach/market_monitor/StockController.java
@@ -0,0 +1,29 @@
+package com.zach.market_monitor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StockController {
+    private final APIService apiService;
+
+    public StockController(APIService apiService) {
+        this.apiService = apiService;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello, world!";
+    }
+
+    @GetMapping("/price2")
+    public String price2(@RequestParam(defaultValue = "AAPL") String symbol) {
+        return apiService.getCurrentPrice(symbol);
+    }
+
+    @GetMapping("/quote")
+    public String quote(@RequestParam(defaultValue = "AAPL") String symbol, @RequestParam(defaultValue = "1day") String interval) {
+        return apiService.getQuote(symbol, interval);
+    }
+}
